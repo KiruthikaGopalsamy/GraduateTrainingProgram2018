3f13dd8ce66eef0f2d23b6bb9112a1dbbc8dd587
KiruthikaGopalsamy <41885199+KiruthikaGopalsamy@users.noreply.github.com>
2018-09-17 12:58:46 +0530

    Update Day4.py

--- Python/Day4.py
+++ Python/Day4.py
@@ -1,44 +1,59 @@
-#1
-
-def sort_backpack():
-    inventory['backpack'].sort()
-    print("sorted list%s"%inventory['backpack'])
-def delete():
-    # del inventory['backpack'][2]
-   inventory['backpack'].pop(2)
-   print("after deletion%s"%inventory['backpack'])	
-def add_element():
-    inventory['gold']=[500,50]
-    print("after adding value to the key gold%s"%inventory)
-inventory = {
-                   'gold' : 500,
-                   'pouch' : ['flint', 'twine', 'gemstone'],
-                   'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
-                }
-				
-inventory['pocket']=['seashell', 'strange berry' ,'lint']
-print("new inventory%s"%inventory)
-sort_backpack()
-delete()
-add_element()
+name=raw_input("enter name")
+new=""
+c_name=name.split(' ')
+for i in range(len(c_name)):
+ new=new+c_name[i].capitalize()+" "
+print new
 """
-#2
+"""
+import string
+name=raw_input("enter name")
+print string.capwords(name)
+"""
 """
 def sum_stu():
-   #sum_s={}
+   sum_s={}
    for k,v in student_details.iteritems():
        sum_s[k]=sum(v)
    print("total mark%s"%sum_s)
-def avg():
+def avg(new):
    avg_s={}
+   k1=new
    for k,v in student_details.iteritems():
-       avg_s[k]=sum(v)/len(v)
-   print("average mark%s"%avg_s)
-
-student_details={}
-student_details['student1']=[99,89,96]
-student_details['student2']=[99,98,96]
-sum_stu()				
-avg()				
-				
+       if(k==k1):
+         print(float(sum(v)/len(v)))
+         #print("average mark%s"%avg_s[k])
+student_details={}	
+std_count=raw_input("enter no student")			
+for i in range(int(std_count)):
+   name=raw_input("enter student name")
+   sub_count=raw_input("enter no of subject")
+   student_marklist=[]
+   for j in range(int(sub_count)):
+      j=j+1
+      v=input("enter marks of subject"+str(j)+":")		
+      student_marklist.append(v)
+   student_details[name]=student_marklist
+	
+print(student_details)
+sum_stu()
+new=raw_input("enter name to find avg")
+avg(new)
 """
+"""
+def div(n1,n2):
+  try:
+   p=n1/n2
+   print(p)
+  except Exception as error:
+    print error
+
+n= raw_input("enter range")
+for i in range(int(n)):
+  n1=raw_input("enter number 1")
+  try:
+     div(int(n1),int(n2))
+  except Exception as error:
+     print error
+  
+ """
