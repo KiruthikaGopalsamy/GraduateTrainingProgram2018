002d3392e26e2858ca3d5bf05d034d764dfed415
KiruthikaGopalsamy <41885199+KiruthikaGopalsamy@users.noreply.github.com>
2018-12-06 10:20:56 +0530

    Delete Solutions.sql

--- SQL/Solutions.sql
+++
@@ -1,8 +0,0 @@
--- ProblemSet<No.>, November <Date> <Year>
--- Submission by <Your Accenture Email.id> 
-
-/* <Question from the ProblemSet> */
-
-< Your SQL Goes Here >
-
-/* Your Record Count from the SQL execution */
